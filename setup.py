437729153a1b7ed1f2d20450fd4b60fefa13c45b
AoyunTech <aoyuntech@163.com>
2021-06-20 14:48:24 +0800

    update

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+ 
+setup(
+    version='0.0.0',
+    scripts=['scripts/pylistener.py','scripts/example.py'],
+    packages=['ayspot_a1_hw'],
+    package_dir={'': 'scripts'}
+)
